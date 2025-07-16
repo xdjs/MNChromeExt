5a976f6cee20b26fcb9f4cebf1efeec20488fd47
Max Lee <maxtacular@icloud.com>
2025-07-16 11:06:24 -0700

    Refactored popup UI to remove unused channel element and improved error handling in artist fetching logic

--- src/frontend/popup/api.js
+++ src/frontend/popup/api.js
@@ -2,7 +2,7 @@ const API = 'https://mn-chrome-ext.vercel.app/';
 
 
 export async function fetchArtist(info) {
-  console.log(info);
+  console.log(info.channel);
   const url = info.channel
       ? `${API}/api/artist/by-id/${encodeURIComponent(info.id)}`
       : `${API}/api/artist/by-user/${encodeURIComponent(info.channel)}`;
--- src/frontend/popup/ui.js
+++ src/frontend/popup/ui.js
@@ -1,8 +1,16 @@
 export function renderArtist(a){
+    
     console.log(a);
     if(!a){ document.body.textContent='Artist not found'; return; }
-    titleEl().textContent = a.name ?? 'Unknown';
-    bioEl().textContent   = a.bio  ?? '';
+
+    console.log(a.bio);
+    const titleEl = document.getElementById('title');
+    const bioEl = document.getElementById('bio');
+
+    titleEl.textContent = a.name ?? 'Unknown';
+    bioEl.textContent = a.bio ?? 'No Bio';
+
+    console.log(titleEl);
+    console.log(bioEl);
   }
-  const titleEl = () => document.getElementById('id');
-  const bioEl   = () => document.getElementById('bio');
+  
