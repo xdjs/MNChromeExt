5ee2d3bb47df13caf6ad428f2a4c6e5ef2ba5fbd
Max Lee <maxtacular@icloud.com>
2025-07-16 11:56:49 -0700

    Updated popup styles for improved layout, added HTML structure, and refactored API calls to streamline artist fetching logic.

--- src/frontend/popup/api.js
+++ src/frontend/popup/api.js
@@ -3,9 +3,14 @@ const API = 'https://mn-chrome-ext.vercel.app/';
 
 export async function fetchArtist(info) {
   console.log(info.channel);
-  const url = info.channel
-      ? `${API}/api/artist/by-id/${encodeURIComponent(info.id)}`
-      : `${API}/api/artist/by-user/${encodeURIComponent(info.channel)}`;
+  const url = `${API}/api/artist/by-id/${encodeURIComponent(info.id)}`
+  const r = await fetch(url);
+  return r.ok ? r.json() : null;
+}
+
+export async function fetchArtistFromName(info) {
+  console.log(info.channel);
+  const url = `${API}/api/artist/by-user/${encodeURIComponent(info.channel)}`;
   const r = await fetch(url);
   return r.ok ? r.json() : null;
 }
