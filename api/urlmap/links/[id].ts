e6dda91e060fe6d5f351e5ffb49bd8a1613621a9
Max Lee <maxtacular@icloud.com>
2025-07-16 17:27:10 -0700

    Modified API handler to correctly reference artist ID instead of artist name

--- api/urlmap/links/[id].ts
+++ api/urlmap/links/[id].ts
@@ -6,12 +6,12 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     res.setHeader('Access-Control-Allow-Headers', 'content-type');
     if (req.method === 'OPTIONS') return res.status(204).end();
 
-    const {artist} = req.query as {artist: string};
-    if (!artist) return res.status(400).json({ error: 'Missing ID' });
+    const {id} = req.query as {id: string};
+    if (!id) return res.status(400).json({ error: 'Missing ID' });
 
     try {
 
-        const a = await getArtistFromId(artist);
+        const a = await getArtistFromId(id);
         if (a) {
             let urls = await getMainUrls(a);
             res.status(200).json(urls ?? {error: 'not found'});
